0af44fe3cda953eade8c3516945a9b20c5405aad
Agung Ys <agungpojokkuburan@gmail.com>
2022-09-16 11:28:29 +0700

    feat : Tugas 2 Agung Ys

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise(
+    (resolve, reject) => {
+      const out = (async()=>{
+        const dataixx = await promiseTheaterIXX();
+        const datavgc = await promiseTheaterVGC();
+        let total = 0;
+        //hitung emosi ixx
+        for(var k in dataixx) {
+          if(dataixx[k].hasil == emosi){
+            total++;
+          }
+        }
+
+        for(var k in datavgc) {
+          if(datavgc[k].hasil == emosi){
+            total++;
+          }
+        }
+        return total;
+      })();
+      resolve(out);
+    }
+  );
+};
 
 module.exports = {
   promiseOutput,
